a4830c4e92df0159ccdd712b6ba0d11dd58ac922
Dongmin <qiandongmin@gmail.com>
2018-04-05 21:44:17 -0700

    Update run.sh file

--- run.sh
+++ run.sh
@@ -3,3 +3,9 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
 #python ./src/sessionization.py ./input/log.csv ./input/inactivity_period.txt ./output/sessionization.txt
+
+GRADER_ROOT=$(dirname ${BASH_SOURCE})
+
+PROJECT_PATH=${GRADER_ROOT}
+
+python ${GRADER_ROOT}/src/sessionization.py
